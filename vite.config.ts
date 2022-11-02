19c9d440c3eb993f32f408fb23651d4a637f191f
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2022-11-02 00:18:22 -0300

    Vitest added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
+/// <reference types="vitest" />
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
 import { HstSvelte } from '@histoire/plugin-svelte'
+import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	histoire: {
 		plugins: [HstSvelte()],
 		setupFile: 'histoire.setup.ts'
-	}
+	},
+	test: {
+		globals: true,
+    	environment: 'jsdom'
+	},
 };
 
 export default config;
