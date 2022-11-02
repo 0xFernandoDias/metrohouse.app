706cd69578b0e23207e9b756e6b7c5474913a021
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2022-11-02 18:17:26 -0300

    Fixed fonts

--- windi.config.ts
+++ windi.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'windicss/helpers'
+import plugin from 'windicss/plugin'
 
 export default defineConfig({
 	shortcuts: {
 		'metro-pink': {
 			'@apply': 'text-metroPink'
 		},
-		'hstack': 'flex items-center',
-		'vstack': 'flex flex-col',
-		'icon': 'w-6 h-6 fill-current',
+		hstack: 'flex items-center',
+		vstack: 'flex flex-col',
+		icon: 'w-6 h-6 fill-current',
 		'app-border': 'border-gray-200 dark:border-dark-300',
 		'app-modal': 'fixed top-0 w-full h-full z-50 bg-white bg-opacity-70 blur-5 shadow-lg'
 	},
@@ -57,7 +58,59 @@ export default defineConfig({
 			},
 			fontFamily: {
 				segoe: ['Segoe UI', 'sans-serif']
-			},
+			}
 		}
-	}
+	},
+	plugins: [
+		plugin(({ addBase }) => {
+			addBase({
+				'@font-face': {
+					fontFamily: 'Segoe UI',
+					src: 'url("/fonts/segoe-ui/segoeuil.ttf") format("truetype")',
+					fontWeight: 'lighter',
+					fontDisplay: 'swap'
+				},
+			})
+		}),
+		plugin(({ addBase }) => {
+			addBase({
+				'@font-face': {
+					fontFamily: 'Segoe UI',
+					src: 'url("/fonts/segoe-ui/segoeuisl.ttf") format("truetype")',
+					fontWeight: 'light',
+					fontDisplay: 'swap'
+				},
+			})
+		}),
+		plugin(({ addBase }) => {
+			addBase({
+				'@font-face': {
+					fontFamily: 'Segoe UI',
+					src: 'url("/fonts/segoe-ui/segoeui.ttf") format("truetype")',
+					fontWeight: 'normal',
+					fontDisplay: 'swap'
+				},
+			})
+		}),
+		plugin(({ addBase }) => {
+			addBase({
+				'@font-face': {
+					fontFamily: 'Segoe UI',
+					src: 'url("/fonts/segoe-ui/seguisb.ttf") format("truetype")',
+					fontWeight: 'bold',
+					fontDisplay: 'swap'
+				},
+			})
+		}),
+		plugin(({ addBase }) => {
+			addBase({
+				'@font-face': {
+					fontFamily: 'Segoe UI',
+					src: 'url("/fonts/segoe-ui/segoeuib.ttf") format("truetype")',
+					fontWeight: 'bolder',
+					fontDisplay: 'swap'
+				},
+			})
+		}),
+	]
 })
