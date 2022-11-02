1ec3bd3a3dbd593121c0f349e8512ac206a6725b
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2022-11-01 23:36:59 -0300

    Sveltekit okay

---
+++ src/routes/Explore/+page.svelte
@@ -0,0 +1,3 @@
+<svelte:head>
+  <title>Explore</title>
+</svelte:head>
--- svelte.config.js
+++ svelte.config.js
@@ -5,11 +5,28 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
 
+	//https://kit.svelte.dev/docs/configuration
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter()
-	}
+	},
+	alias: {},
+	env: {
+		dir: process.cwd(),
+		publicPrefix: 'PUBLIC_'
+	  },
+	  files: {
+		assets: 'static',
+		tests: 'tests',
+		lib: 'src/lib',
+		params: 'src/params',
+		routes: 'src/routes',
+		serviceWorker: 'src/service-worker',
+		appTemplate: 'src/app.html',
+		errorTemplate: 'src/error.html'
+	  },
+	extensions: ['.svelte'],
 };
 
 export default config;
