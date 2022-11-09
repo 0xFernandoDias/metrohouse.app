875eadcadb8440d3e42afbb0e490d7e859ac3335
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2022-11-09 19:10:25 -0300

    Secondary text

--- src/lib/components/basicInputs/Button/Button.svelte
+++ src/lib/components/basicInputs/Button/Button.svelte
@@ -2,9 +2,9 @@
 	export let appearance: 'primary' | 'secondary' | 'textButton' | 'transparent' = 'primary'
 </script>
 
-<div class="flex max-w-min justify-center items-center rounded-sm {appearance}">
+<div class="flex min-w-max justify-center items-center rounded-sm {appearance}">
 	<button type="button" class={appearance}>
-		<svg class="flex min-h-5 w-4" viewBox="0 0 22 22">
+		<svg viewBox="0 0 22 22">
 			<circle
 				cx="10"
 				cy="10"
@@ -17,8 +17,11 @@
 				transform="translate(1 1)"
 			/>
 		</svg>
-		<span class="smSemibold first-letter:uppercase">rest</span>
-		<!-- <svg class="flex min-h-5 w-4" viewBox="0 2 14 1">
+		<div class="flex flex-col items-start">
+			<span class="smSemibold first-letter:uppercase">rest</span>
+			<span class="xsRegular first-letter:uppercase">this is a secondary text</span>
+		</div>
+		<!-- <svg viewBox="0 2 14 1">
 			<path
 				fill="none"
 				stroke-linecap="round"
@@ -30,7 +33,7 @@
 	</button>
 	<button type="button" class={appearance}>
 		<div class="flex h-4 w-[1px] bg-grey60" />
-		<svg class="flex min-h-5 w-4" viewBox="0 2 14 1">
+		<svg viewBox="0 2 14 1">
 			<path
 				fill="none"
 				stroke-linecap="round"
@@ -43,6 +46,44 @@
 </div>
 
 <style>
+	.primary,
+	.secondary,
+	.textButton {
+		@apply shadow-sm;
+		button {
+			@apply disabled:bg-grey20;
+		}
+	}
+
+	.primary button,
+	.secondary button,
+	.textButton button,
+	.transparent button {
+		@apply disabled:text-grey90;
+		&:disabled {
+			svg circle,
+			svg path {
+				@apply stroke-grey90;
+			}
+		}
+	}
+
+	.secondary button,
+	.textButton button,
+	.transparent button {
+		span {
+			@apply first:text-grey190 last:text-grey130;
+		}
+		svg path {
+			@apply stroke-grey130;
+		}
+	}
+
+	.secondary button,
+	.textButton button {
+		@apply hover:bg-grey20 active:bg-grey30;
+	}
+
 	.primary button {
 		@apply bg-themePrimary text-white hover:bg-themeDarkAlt active:bg-themeDark;
 		svg circle,
@@ -56,11 +97,6 @@
 		svg circle {
 			@apply stroke-grey190;
 		}
-	}
-
-	.secondary button,
-	.textButton button {
-		@apply hover:bg-grey20 active:bg-grey30;
 	}
 
 	.transparent button {
@@ -81,37 +117,6 @@
 		}
 	}
 
-	.primary button,
-	.secondary button,
-	.textButton button,
-	.transparent button {
-		@apply disabled:text-grey90;
-		&:disabled {
-			svg circle,
-			svg path {
-				@apply stroke-grey90;
-			}
-		}
-	}
-
-	.primary,
-	.secondary,
-	.textButton {
-		@apply shadow-sm;
-		button {
-			@apply disabled:bg-grey20;
-		}
-	}
-
-	.secondary button,
-	.textButton button,
-	.transparent button {
-		@apply text-grey190;
-		svg path {
-			@apply stroke-grey130;
-		}
-	}
-
 	.textButton button svg circle,
 	.transparent button svg circle {
 		@apply stroke-themePrimary;
@@ -119,5 +124,9 @@
 
 	button {
 		@apply flex py-1.5 gap-2 justify-center items-center first:px-5 rounded-l-sm last:pr-2 rounded-r-sm;
+	}
+
+	svg {
+		@apply flex min-h-5 w-4;
 	}
 </style>
