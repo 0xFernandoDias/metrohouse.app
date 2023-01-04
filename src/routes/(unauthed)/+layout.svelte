9637709cbef26795ef32b898950b280767289c25
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2023-01-04 08:33:54 -0300

    checkpoint

--- src/routes/(unauthed)/+layout.svelte
+++ src/routes/(unauthed)/+layout.svelte
@@ -1,42 +1,12 @@
 <script lang="ts">
 	import { client as clientLens } from '$lib/lensCall'
-	import { getContextClient, gql, queryStore, setContextClient } from '@urql/svelte'
+	import { setContextClient } from '@urql/svelte'
 
 	export let data: any
 
 	setContextClient(clientLens)
 
-	const ProfileQuery = gql`
-		query Profile {
-			profile(request: { profileId: "0x35" }) {
-				id
-				name
-				bio
-				picture {
-					... on MediaSet {
-						original {
-							url
-						}
-					}
-				}
-				ownedBy
-			}
-		}
-	`
-
-	let returnedProfile = queryStore({
-		client: getContextClient(),
-		query: ProfileQuery
-	})
-
-	$: {
-		if (!$returnedProfile.fetching && $returnedProfile.data !== undefined) {
-			returnedProfile = $returnedProfile.data
-		}
-	}
-
 	$: console.log('data ++layout reactive: ', data)
-	$: console.log('returnedProfile ++layout reactive: ', $returnedProfile) // fetching: true
 </script>
 
 <slot />
--- src/routes/(unauthed)/+page.svelte
+++ src/routes/(unauthed)/+page.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
+	import { gql, queryStore, getContextClient } from '@urql/svelte'
+
+	const ProfileQuery = gql`
+		query Profile {
+			profile(request: { profileId: "0x35" }) {
+				id
+				name
+				bio
+				picture {
+					... on MediaSet {
+						original {
+							url
+						}
+					}
+				}
+				ownedBy
+			}
+		}
+	`
+
+	const returnedProfile = queryStore({
+		client: getContextClient(),
+		query: ProfileQuery
+	})
+
 	export let data: any
 
 	$: console.log('data ++page reactive', data)
+	$: console.log('returnedProfile ++layout reactive: ', $returnedProfile) // fetching: true
 </script>
 
 <svelte:head>
@@ -10,6 +36,17 @@
 </svelte:head>
 
 <div>Hi</div>
+<section>
+	{#if $returnedProfile.fetching}
+		...loading
+	{:else if $returnedProfile.error}
+		...error
+	{:else}
+		{$returnedProfile.data.profile.name}
+		{$returnedProfile.data.profile.bio}
+		{$returnedProfile.data.profile.ownedBy}
+	{/if}
+</section>
 
 <style>
 </style>
